d03d355a1503fc7973e6a2c667d19cc60363580d
kelvincheung000 <cheungkamkit@gmail.com>
2025-05-05 21:21:49 +0800

    Updated data fetching logic; code refactoring

---
+++ build/CMakeFiles/backtester.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/kelvin_cheung/Documents/Quant_Trading/Crypto_Trading_Strategy_Backtester/src/backtester/backtester.cpp" "CMakeFiles/backtester.dir/src/backtester/backtester.cpp.o" "gcc" "CMakeFiles/backtester.dir/src/backtester/backtester.cpp.o.d"
+  "/Users/kelvin_cheung/Documents/Quant_Trading/Crypto_Trading_Strategy_Backtester/src/backtester/performance_metrics.cpp" "CMakeFiles/backtester.dir/src/backtester/performance_metrics.cpp.o" "gcc" "CMakeFiles/backtester.dir/src/backtester/performance_metrics.cpp.o.d"
+  "/Users/kelvin_cheung/Documents/Quant_Trading/Crypto_Trading_Strategy_Backtester/src/data/data_loader.cpp" "CMakeFiles/backtester.dir/src/data/data_loader.cpp.o" "gcc" "CMakeFiles/backtester.dir/src/data/data_loader.cpp.o.d"
+  "/Users/kelvin_cheung/Documents/Quant_Trading/Crypto_Trading_Strategy_Backtester/src/main.cpp" "CMakeFiles/backtester.dir/src/main.cpp.o" "gcc" "CMakeFiles/backtester.dir/src/main.cpp.o.d"
+  "/Users/kelvin_cheung/Documents/Quant_Trading/Crypto_Trading_Strategy_Backtester/src/strategies/bollinger_bands_strategy.cpp" "CMakeFiles/backtester.dir/src/strategies/bollinger_bands_strategy.cpp.o" "gcc" "CMakeFiles/backtester.dir/src/strategies/bollinger_bands_strategy.cpp.o.d"
+  "/Users/kelvin_cheung/Documents/Quant_Trading/Crypto_Trading_Strategy_Backtester/src/strategies/rsi_strategy.cpp" "CMakeFiles/backtester.dir/src/strategies/rsi_strategy.cpp.o" "gcc" "CMakeFiles/backtester.dir/src/strategies/rsi_strategy.cpp.o.d"
+  "/Users/kelvin_cheung/Documents/Quant_Trading/Crypto_Trading_Strategy_Backtester/src/strategies/sma_strategy.cpp" "CMakeFiles/backtester.dir/src/strategies/sma_strategy.cpp.o" "gcc" "CMakeFiles/backtester.dir/src/strategies/sma_strategy.cpp.o.d"
+  "/Users/kelvin_cheung/Documents/Quant_Trading/Crypto_Trading_Strategy_Backtester/src/strategies/strategy.cpp" "CMakeFiles/backtester.dir/src/strategies/strategy.cpp.o" "gcc" "CMakeFiles/backtester.dir/src/strategies/strategy.cpp.o.d"
+  "/Users/kelvin_cheung/Documents/Quant_Trading/Crypto_Trading_Strategy_Backtester/src/utils/csv_utils.cpp" "CMakeFiles/backtester.dir/src/utils/csv_utils.cpp.o" "gcc" "CMakeFiles/backtester.dir/src/utils/csv_utils.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/backtester.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/backtester.dir/src/backtester/backtester.cpp.o"
+  "CMakeFiles/backtester.dir/src/backtester/backtester.cpp.o.d"
+  "CMakeFiles/backtester.dir/src/backtester/performance_metrics.cpp.o"
+  "CMakeFiles/backtester.dir/src/backtester/performance_metrics.cpp.o.d"
+  "CMakeFiles/backtester.dir/src/data/data_loader.cpp.o"
+  "CMakeFiles/backtester.dir/src/data/data_loader.cpp.o.d"
+  "CMakeFiles/backtester.dir/src/main.cpp.o"
+  "CMakeFiles/backtester.dir/src/main.cpp.o.d"
+  "CMakeFiles/backtester.dir/src/strategies/bollinger_bands_strategy.cpp.o"
+  "CMakeFiles/backtester.dir/src/strategies/bollinger_bands_strategy.cpp.o.d"
+  "CMakeFiles/backtester.dir/src/strategies/rsi_strategy.cpp.o"
+  "CMakeFiles/backtester.dir/src/strategies/rsi_strategy.cpp.o.d"
+  "CMakeFiles/backtester.dir/src/strategies/sma_strategy.cpp.o"
+  "CMakeFiles/backtester.dir/src/strategies/sma_strategy.cpp.o.d"
+  "CMakeFiles/backtester.dir/src/strategies/strategy.cpp.o"
+  "CMakeFiles/backtester.dir/src/strategies/strategy.cpp.o.d"
+  "CMakeFiles/backtester.dir/src/utils/csv_utils.cpp.o"
+  "CMakeFiles/backtester.dir/src/utils/csv_utils.cpp.o.d"
+  "backtester"
+  "backtester.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/backtester.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
